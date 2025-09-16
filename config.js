8aed7217a721b40afae600da34c5d03e67961f8e
DominicDev <s0l0estudi4r@gmail.com>
2025-09-16 11:11:20 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' FRÁGIL';
+export const nameSongs = [
+	"AV. LARCO", /*1*/
+	"OBERTURA", /*2*/
+	"MUNDO RARO", /*3*/
+	"PASTAS, PELOTAS Y OTROS POSTRES", /*4*/
+	"ESTO ES ILUMINACIÓN", /*5*/
+	"FLORAL", /*6*/
+	"EL CAIMÁN", /*7*/
+	"ODA AL TULIPÁN", /*8*/
+	"LIZY", /*9*/
+	"LE DICEN ROCK", /*10*/
+	"EL ABUELO", /*11*/
+	"ANIMALES", /*12*/
+	"CARAS", /*13*/
+	"FOTOGRAMA", /*14*/
+	"SORPRESA DEL TIEMPO", /*15*/
+	"ALUNADO", /*16*/
+	"BASTA YA", /*17*/
+	"HECHA DE RAYOS DE SOL", /*18*/
+	"NO NOS PUEDEN PARAR", /*19*/
+	"AFERRARME A TI", /*20*/
+]
